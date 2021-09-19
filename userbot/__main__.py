f2ac0808f90969df9e4e941240b2917145e16a4b
aman706 <76611203+aman706@users.noreply.github.com>
2021-09-19 10:41:38 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,16 +1,11 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot start point """
+
 
 from importlib import import_module
 from sys import argv
 
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import BOT_VER, LOGS, bot
-from userbot.modules import ALL_MODULES
+from Ironx import BOT_VER, LOGS, bot
+from Ironx.modules import ALL_MODULES
 
 INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
              '\n Tip: Use Country Code along with number.' \
@@ -23,11 +18,11 @@ except PhoneNumberInvalidError:
     exit(1)
 
 for module_name in ALL_MODULES:
-    imported_module = import_module("userbot.modules." + module_name)
+    imported_module = import_module("Ironx.modules." + module_name)
 
 
 LOGS.info(
-    f"⚡Geez - Project⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"⚡Ironx - Project⚡ ⚙️ V{BOT_VER} [HAS BEEN ACTIVATED!!]")
 
 
 if len(argv) not in (1, 3, 4):
